942e583746cbdd5c0b15f97e68cb3d1485f04abf
Jonathan Pobst <monkey@jpobst.com>
2010-07-28 11:56:42 -0500

    Add generic utility class.

---
+++ Helpers/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWrench
+{
+	static class Utilities
+	{
+		public static string FormatRevision (string revision)
+		{
+			if (string.IsNullOrEmpty (revision))
+				return revision;
+
+			// This is likely a SVN revision
+			if (revision.Length <= 6)
+				return string.Format ("r{0}", revision);
+
+			// This is likely a GIT revision
+			return revision.Substring (0, 7);
+		}
+	}
+}
